63ce3696c5fdb1b369e4feadc0fc84658b7518b8
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 06:43:23 -0500

    Server - Adding dashboard route

--- code-challenge/server/routes/index.js
+++ code-challenge/server/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const indexController = require('../controllers/index')
 const authController = require('../controllers/auth')
+const dashboardController = require('../controllers/dashboard')
 
-router.post('/signup', authController.handleSignUp)
+router.post('/signup', authController.handleSignup)
+router.post('/dashboard', dashboardController.loadDashboard)
 
 module.exports = router
