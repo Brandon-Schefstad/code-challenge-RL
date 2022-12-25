b6c9de88a8286b11f83b0101c52578e10f7c43e9
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 04:22:00 -0500

    client - Added signup form

--- code-challenge/frontend/src/components/SignUp.jsx
+++ code-challenge/frontend/src/components/SignUp.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const SignUp = () => {
-	return <div>SignUp</div>
+	return (
+		<>
+			<form action="/signup" method="GET">
+				<label htmlFor="email">Email</label>
+				<input type="text" name="email" />
+				<label htmlFor="password">password</label>
+				<input type="text" name="password" />
+				<label htmlFor="confirm">confirm password</label>
+				<input type="text" name="confirm" />
+				<input type="button" value="Submit" />
+			</form>
+		</>
+	)
 }
 
 export default SignUp
