95a496b9d3bd43f739633df833010aa1fac91cbe
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 15:09:13 -0500

    Client - Form for posting new todo

--- code-challenge/client/src/components/Dashboard.jsx
+++ code-challenge/client/src/components/Dashboard.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import { Link, Navigate } from 'react-router-dom'
 import useAuth from './hooks/useAuth'
@@ -7,9 +8,28 @@ const Dashboard = () => {
 	if (!user) {
 		return <Navigate to="/" replace="true" />
 	}
+	async function postTodo(e) {
+		e.preventDefault()
+		await axios
+			.post('/todo/postTodo', {
+				todo: document.getElementById('todo').value,
+				finished: document.getElementById('finished').value,
+			})
+			.then((res) => {
+				console.log(res)
+			})
+	}
 	return (
 		<>
 			<div>{user}</div>
+			<form onSubmit={postTodo}>
+				<label htmlFor="todo"></label>
+				<input type="text" name="todo" id="todo" />
+				<label htmlFor="finished"></label>
+				<input type="checkbox" name="finished" id="finished" />
+				<input type="submit" value="Add Todo" />
+			</form>
+
 			<button onClick={() => useAuth('remove')}>
 				<Link to="/">Logout</Link>
 			</button>
