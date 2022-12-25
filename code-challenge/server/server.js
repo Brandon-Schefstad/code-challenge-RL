bc85e2d90449d31b3e40024328f6921b54893299
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 06:42:32 -0500

    Server -updated server dependencies

--- code-challenge/server/server.js
+++ code-challenge/server/server.js
@@ -1,13 +1,48 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')
+const passport = require('passport')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const flash = require('express-flash')
 
 const indexRoutes = require('./routes/index.js')
-
+app.use(flash())
 app.use(bodyParser.json())
 require('dotenv').config({ path: '.env' })
 app.listen(process.env.PORT || 8000, () => {
 	console.log(`http://localhost:${process.env.PORT}`)
 })
-// app.use(express.static(__dirname + '/views'))
+
+/**Session configs */
+app.use(
+	session({
+		store: MongoStore.create({
+			mongoUrl: process.env.MONGO_URI,
+		}),
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+	})
+)
+require('./middleware/passport')(passport)
+/**Passport Middleware*/
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(methodOverride('_method'))
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI)
+		console.log(`MongoDB Connected: ${conn.connection.host}`)
+	} catch (err) {
+		console.error(err)
+		process.exit(1)
+	}
+}
+/**Passport */
+
+connectDB()
 app.use('/', indexRoutes)
