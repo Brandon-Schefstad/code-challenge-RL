ea292618787f1ca3377169c831f0fb5b15a881cd
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 05:46:34 -0500

    Client - Form to make POST to /signup

--- code-challenge/client/src/components/SignUp.jsx
+++ code-challenge/client/src/components/SignUp.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmPassword, setConfirmPassword] = useState('')
+
+	const updateEmail = (e) => setEmail(e.target.value)
+	const updatePassword = (e) => setPassword(e.target.value)
+	const matchPassword = (e) => setConfirmPassword(e.target.value === password)
+
+	async function postSignUp(e) {
+		e.preventDefault()
+		await axios
+			.post('/signup', { email: email, password: password })
+			.then(function (response) {
+				console.log(response)
+			})
+	}
+
 	return (
 		<>
-			<form action="/signup" method="GET">
+			<form onSubmit={postSignUp}>
 				<label htmlFor="email">Email</label>
-				<input type="text" name="email" />
+				<input type="text" name="email" onChange={updateEmail} />
 				<label htmlFor="password">password</label>
-				<input type="text" name="password" />
+				<input type="text" name="password" onChange={updatePassword} />
 				<label htmlFor="confirm">confirm password</label>
-				<input type="text" name="confirm" />
-				<input type="button" value="Submit" />
+				<input type="text" name="confirm" onChange={matchPassword} />
+				<input type="submit" value="Submit" />
 			</form>
 		</>
 	)
