8a3da6abdb2f5cb425c2ab152128d4f60b56b156
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 05:46:01 -0500

    Server- implementing body-parser

--- code-challenge/server/server.js
+++ code-challenge/server/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const indexRoutes = require('./routes/index.js')
+
+app.use(bodyParser.json())
 require('dotenv').config({ path: '.env' })
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
 	console.log(`http://localhost:${process.env.PORT}`)
 })
 // app.use(express.static(__dirname + '/views'))
-app.get('/', indexRoutes)
+app.use('/', indexRoutes)
