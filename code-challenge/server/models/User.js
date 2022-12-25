abf04b6a8ce828aeaf0810f2238538d114f3f051
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 04:19:15 -0500

    server - Set up User model

---
+++ code-challenge/server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	todos: [
+		{
+			text: {
+				type: String,
+				required: true,
+			},
+			date: {
+				type: Date,
+				default: Date.now(),
+			},
+		},
+	],
+})
+
+module.exports = mongoose.model('User', User)
