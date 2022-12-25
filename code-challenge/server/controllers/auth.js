b754218502102d1728b1f217e0b032cbda59a89f
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-25 06:43:55 -0500

    Server: Adding signup functionality

--- code-challenge/server/controllers/auth.js
+++ code-challenge/server/controllers/auth.js
@@ -1,6 +1,60 @@
+const passport = require('passport')
+const validator = require('validator')
+const flash = require('express-flash')
+const User = require('../models/User')
+
 module.exports = {
-	async handleSignUp(req, res) {
+	handleSignup(req, res, next) {
 		console.log(req.body)
-		res.json({ Hello: 'World' })
+		const validationErrors = []
+		if (!validator.isEmail(req.body.email))
+			validationErrors.push({ msg: 'Please enter a valid email address.' })
+		if (!validator.isLength(req.body.password, { min: 8 }))
+			validationErrors.push({
+				msg: 'Password must be at least 8 characters long',
+			})
+		if (req.body.password !== req.body.confirmPassword)
+			validationErrors.push({ msg: 'Passwords do not match' })
+
+		if (validationErrors.length) {
+			console.log(validationErrors)
+			return res.redirect('/signup')
+		}
+		req.body.email = validator.normalizeEmail(req.body.email, {
+			gmail_remove_dots: false,
+		})
+
+		const user = new User({
+			email: req.body.email,
+			password: req.body.password,
+			todos: [],
+		})
+		console.log(req.logIn)
+		User.findOne(
+			{ email: req.body.email },
+
+			(err, existingUser) => {
+				if (err) {
+					return next(err)
+				}
+				if (existingUser) {
+					req.flash('errors', {
+						msg: 'Account with that email address or username already exists.',
+					})
+					return res.redirect('../signup')
+				}
+				user.save((err) => {
+					if (err) {
+						return next(err)
+					}
+					req.logIn(user, (err) => {
+						if (err) {
+							return next(err)
+						}
+						res.redirect('/dashboard')
+					})
+				})
+			}
+		)
 	},
 }
--- code-challenge/server/models/User.js
+++ code-challenge/server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const User = new mongoose.Schema({
 	email: {
@@ -23,4 +24,29 @@ const User = new mongoose.Schema({
 	],
 })
 
+// Password hash middleware.
+User.pre('save', function save(next) {
+	const user = this
+	if (!user.isModified('password')) {
+		return next()
+	}
+	bcrypt.genSalt(10, (err, salt) => {
+		if (err) {
+			return next(err)
+		}
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			if (err) {
+				return next(err)
+			}
+			user.password = hash
+			next()
+		})
+	})
+})
+
+User.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+		cb(err, isMatch)
+	})
+}
 module.exports = mongoose.model('User', User)
